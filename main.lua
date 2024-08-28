a517a8d84eb5a88f6a8c91fa975a93cff773d1fa
Rodrigo Pupo <109440284+RoPupo@users.noreply.github.com>
2024-08-28 17:56:08 -0300

    - Novas funções

--- main.lua
+++ main.lua
@@ -1,5 +1,19 @@
---- Nova função. Como é criada? O que pensar?
-function novaFuncao()
-   
+
+-- Nova função, sem parametros e sem retorno.
+
+local function novaFuncao()
+   os.execute("chcp 65001")
+   print("Olá, sou uma nova função !")
 end
 
+local function kmMph(velocidadeKm)
+    return velocidadeKm / 1.6
+end
+
+local function soma(a, b)
+    resulta = a + b
+    return resultado
+end
+a = 10
+b = 100
+print(soma( a + b))
